cacb24faa086bb66b32fe319bbe2757f8ba5fe3b
Manuel <97695681+manudelu@users.noreply.github.com>
2024-02-21 14:08:50 +0000

    Add files via upload

---
+++ Assignment_SoRo/assignment_coreographe/assignment_2_coreographe.pml
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="assignment_2_coreographe" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs>
+        <Dialog name="target" src="target/target.dlg" />
+        <Dialog name="bring" src="bring/bring.dlg" />
+        <Dialog name="ExampleDialog" src="behavior_1/ExampleDialog/ExampleDialog.dlg" />
+        <Dialog name="Lego" src="Lego/Lego.dlg" />
+    </Dialogs>
+    <Resources />
+    <Topics>
+        <Topic name="target_enu" src="target/target_enu.top" topicName="target" language="en_US" />
+        <Topic name="bring_enu" src="bring/bring_enu.top" topicName="bring" language="en_US" />
+        <Topic name="ExampleDialog_enu" src="behavior_1/ExampleDialog/ExampleDialog_enu.top" topicName="ExampleDialog" language="en_US" />
+        <Topic name="Lego_enu" src="Lego/Lego_enu.top" topicName="Lego" language="en_US" />
+    </Topics>
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
---
+++ Assignment_SoRo/assignment_coreographe/translations/translation_en_US.ts
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Hi matteo, how are you?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hi matteo, how are you?</translation>
+        </message>
+        <message>
+            <source>Hi manuel, I am Pepper nice to meet you! How can I help you?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hi manuel, I am Pepper nice to meet you! How can I help you?</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello Manuel, nice to meet you. Simone told me you need a hand.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello Manuel, nice to meet you. Simone told me you need a hand.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hi simone! I have something for you!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hi simone! I have something for you!</translation>
+        </message>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Thanks, let's go to Simone now!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Thanks, let's go to Simone now!</translation>
+        </message>
+        <message>
+            <source>Thanks, let's go to Matteo now!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Thanks, let's go to Matteo now!</translation>
+        </message>
+        <message>
+            <source>Hello Matteo, your friends told me you needed a hand finishing your game.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello Matteo, your friends told me you needed a hand finishing your game.</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello Matteo, your friend told me you needed this to finish your game.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello Matteo, your friend told me you needed this to finish your game.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>Hi Simone, I have something for you!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hi Simone, I have something for you!</translation>
+        </message>
+        <message>
+            <source>Hi Matteo, I have something for you!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hi Matteo, I have something for you!</translation>
+        </message>
+        <message>
+            <source>Hello Matteo, your friends told me you needed a hand finishing your game.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello Matteo, your friends told me you needed a hand finishing your game.</translation>
+        </message>
+    </context>
+</TS>
